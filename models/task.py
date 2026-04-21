9a9bd593e7e343caeb7732cc605c090e1e86a0be
hariom9916 <94966974+hariom9916@users.noreply.github.com>
2026-04-22 02:35:52 +0530

    Update task.py

--- models/task.py
+++ models/task.py
@@ -1,12 +1,15 @@
-class Task:
-    def __init__(self, title, deadline, priority="medium"):
-        self.title = title
-        self.deadline = deadline
-        self.priority = priority
-        self.status = "pending"
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    deadline = db.Column(db.String(50))
+    priority = db.Column(db.String(20))
+    status = db.Column(db.String(20))
 
     def to_dict(self):
         return {
+            "id": self.id,
             "title": self.title,
             "deadline": self.deadline,
             "priority": self.priority,
