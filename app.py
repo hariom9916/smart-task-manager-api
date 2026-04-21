7919ebca0bdf796b0b54b4759963bca4b3f7bdfb
hariom9916 <94966974+hariom9916@users.noreply.github.com>
2026-04-22 02:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes.task_routes import task_bp
 
 app = Flask(__name__)
 
+# Database config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Make db available everywhere
+from models.task import Task
+
 app.register_blueprint(task_bp)
+
+@app.route("/")
+def home():
+    return "Smart Task Manager API is running 🚀"
+
+# Create DB tables
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
