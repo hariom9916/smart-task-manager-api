f6e2abd1172e369efaa5f6f5a00cdc7bea66e214
hariom9916 <94966974+hariom9916@users.noreply.github.com>
2026-04-22 02:12:32 +0530

    Add auto_priority function to determine task priority

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,7 @@
-
+def auto_priority(deadline):
+    if "today" in deadline.lower():
+        return "high"
+    elif "tomorrow" in deadline.lower():
+        return "medium"
+    else:
+        return "low"
